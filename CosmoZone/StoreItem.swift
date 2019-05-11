13b51db249e25b47e1373060401fed46f2923425
Volodymyr <klymenkodp@gmail.com>
2019-05-10 23:51:26 -0400

    Create StoreItem class

---
+++ CosmoZone/StoreItem.swift
@@ -0,0 +1,16 @@
+//
+//  StoreItem.swift
+//  CosmoZone
+//
+//  Created by Volodymyr Klymenko on 2019-05-10.
+//  Copyright © 2019 Volodymyr Klymenko. All rights reserved.
+//
+
+import UIKit
+
+class StoreItem: UIView {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+
+}
